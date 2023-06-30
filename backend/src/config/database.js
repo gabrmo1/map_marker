1ba868ca0753937cdc50acfd13766d110576db3e
gabrmo1 <gabrielmoreno1jlle@gmail.com>
2023-06-30 13:25:00 -0300

    created slinter rules, refactored the code to meet the rules

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,17 +1,24 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:Maps123!@db:5432/maps', {
-    host: 'db',
-    dialect: 'postgres',
+const dialect = 'postgres';
+const host = 'db';
+const port = '5432';
+const database = 'maps';
+const user = 'postgres';
+const password = 'Maps123!';
+
+const sequelize = new Sequelize(`${dialect}://${user}:${password}@${host}:${port}/${database}`, {
+  host,
+  dialect,
 });
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connected to the database');
-    })
-    .catch((error) => {
-        console.error('Unable to connect to the database:', error);
-    });
+  .authenticate()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 module.exports = { sequelize };
