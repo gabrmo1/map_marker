f99efe27291fb18a4f4ba7224481dce98a286152
gabrmo1 <gabrielmoreno1jlle@gmail.com>
2023-06-30 12:24:45 -0300

    created slinter rules, refactored the code to meet the rules and created custom markers

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,7 @@
 export function doFetch(url, method) {
-    return fetch(url, { method: method, headers: { 'Content-Type': 'application/json' } })
+  return fetch(url, { method, headers: { 'Content-Type': 'application/json' } });
 }
 
 export function doFetchBody(url, method, body) {
-    return fetch(url, { method: method, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) })
+  return fetch(url, { method, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) });
 }
